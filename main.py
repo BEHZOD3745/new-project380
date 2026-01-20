30dda33ff9f67d546b5dcae2558070147983b1f7
BEHZOD3745 <rahmonberdiev3745@gmail.com>
2026-01-20 15:20:44 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+A = float(input("Введите A: "))
+B = float(input("Введите B: "))
+C = float(input("Введите C: "))
+
+if (A<B<C) or (A>B>C):
+    A*= 2
+    B*= 2
+    C*= 2
+
+else :
+    A = -A
+    B = -B
+    C = -C
+
+print("A =", A)
+print("B =", B)
+print("C =", C)
